5e0e356f55cb3b3517a21a1ccf894ff15b752404
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-09-14 18:50:55 -0300

    Code documentation Generalizes the download of categories/subcategories

--- dump.py
+++ dump.py
@@ -1,10 +1,11 @@
 #TODO: interpret root categories
-
+# IMPORTS =====================================================================
 import urllib.request, urllib.error, urllib.parse
 import json
 
+# CONSTANTS ============================================================================================================
 BASE_ALEXA_SITE =       "https://www.alexa.com"
-SUBCATEG_TITLE_BEGIN =  "<a class=\"catList open\">Sub-Categories"
+SUBCATEG_TITLE_BEGIN =  "<div class=\"categories"
 SUBCATEG_TITLE_END =    "</section>"
 SUBCATEG_ITEM_BEGIN =   "<li><a href=\""
 SUBCATEG_ITEM_END =     "</a> <span class=\"small gray\""
@@ -16,6 +17,7 @@ SITE_ITEM_END_1 =       "\">"
 FILE_TO_SAVE =          "data.json"
 THRESHOLD_TO_SAVE =     100
 
+# FUNCTIONS ============================================================================================================
 def web_page_to_text(url):
     text_return = ""
     try:
@@ -27,7 +29,7 @@ def web_page_to_text(url):
         pass
     return text_return
 
-def alexa_get_subcateg_page(sufix):
+def alexa_get_subcateg(sufix):
     list_subcateg = []
     content = web_page_to_text(BASE_ALEXA_SITE + sufix)
     position = content.find(SUBCATEG_TITLE_BEGIN)
@@ -42,7 +44,7 @@ def alexa_get_subcateg_page(sufix):
             list_subcateg.append(content[0:position].split(SUBCATEG_ITEM_SPLIT)[0])
     return list_subcateg
 
-def alexa_get_sites_page(sufix):
+def alexa_get_sites(sufix):
     list_sites = []
     content = web_page_to_text(BASE_ALEXA_SITE + sufix)
     while content.find(SITE_ITEM_BEGIN_0) != -1:
@@ -57,30 +59,33 @@ def alexa_get_sites_page(sufix):
         list_sites.append(raw_item)
     return list_sites
 
-#Loads existent file
+# MAIN SCRIPT ==========================================================================================================
+# Loads existent file
 try:
     fp = open(FILE_TO_SAVE, "r")
     all_data = json.load(fp)
     fp.close()
 except:
     all_data = {}
+# Control
 dont_stress_the_disk = 0
-list_to_explore = ["/topsites/category/Top/Games/Video_Games", "/topsites/category/Top/Games/Yard,_Deck,_and_Table_Games"]
-
+# Stack as list
+list_to_explore = ["/topsites/category/Top"]
+#Depth first algorithm
 while len(list_to_explore) > 0:
-    item_to_explore = list_to_explore.pop(0)
+    item_to_explore = list_to_explore.pop(0) #Stack control
     print("")
     print("Exploring: " + item_to_explore)
     print("List size: " + str(len(list_to_explore)))
-    explored_result = alexa_get_subcateg_page(item_to_explore)
-    if len(explored_result) == 0:
+    explored_result = alexa_get_subcateg(item_to_explore)
+    if len(explored_result) == 0: #End node
         hierarchical_key_list = item_to_explore.split("/")
         hierarchical_key_list.remove("")
         curr_dict = all_data
         for i in range(len(hierarchical_key_list)):
             if hierarchical_key_list[i] not in curr_dict.keys():
                 if i == len(hierarchical_key_list) - 1:
-                    curr_dict[hierarchical_key_list[i]] = alexa_get_sites_page(item_to_explore)
+                    curr_dict[hierarchical_key_list[i]] = alexa_get_sites(item_to_explore)
                     print("Found sites: " + str(len(curr_dict[hierarchical_key_list[i]])))
                     dont_stress_the_disk += 1
                     print("End-points until disk record: " + str(THRESHOLD_TO_SAVE - dont_stress_the_disk))
@@ -93,6 +98,6 @@ while len(list_to_explore) > 0:
                     curr_dict[hierarchical_key_list[i]] = {}
             curr_dict = curr_dict[hierarchical_key_list[i]] #Shallow copy
         
-    else:
+    else: #Parent node
         list_to_explore = explored_result + list_to_explore
 
