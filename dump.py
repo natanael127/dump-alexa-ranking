46e319138dc6429630ad0d94698ef8d5baa3f885
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-09-16 21:58:46 -0300

    Creates json file initialization

--- dump.py
+++ dump.py
@@ -14,7 +14,7 @@ SITE_ITEM_BEGIN_0 =     "<div class=\"tr site-listing\">"
 SITE_ITEM_END_0 =       "</p>"
 SITE_ITEM_BEGIN_1 =     "<a href=\"/siteinfo/"
 SITE_ITEM_END_1 =       "\">"
-FILE_TO_SAVE =          "data.json"
+FILE_OUTPUT =           "data.json"
 LOCAL_PATH_PREFIX =     "./"
 THRESHOLD_TO_SAVE =     100
 
@@ -61,11 +61,20 @@ def alexa_get_sites_offline(content):
 def open_creating_dirs(path, mode):
     if not os.path.isdir(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
-    fpx = open(path, mode)
-    return fpx
+    fp_itemx = open(path, mode)
+    return fp_itemx
 
 # MAIN SCRIPT ==========================================================================================================
-# Control
+# Output file initialization
+if os.path.exists(FILE_OUTPUT):
+    fp_json = open(FILE_OUTPUT, "r")
+    output_data = json.load(fp_json)
+else:
+    output_data = {}
+    fp_json = open(FILE_OUTPUT, "w")
+    json.dump(output_data, fp_json)
+fp_json.close()
+# Controls
 dont_stress_the_disk = 0
 always_increment = 0
 visited_list = []
@@ -73,7 +82,7 @@ visited_list = []
 list_to_explore = ["/topsites/category/Top"]
 #Depth first algorithm
 while len(list_to_explore) > 0:
-    item_to_explore = list_to_explore.pop(0) #Stack control
+    item_to_explore = list_to_explore.pop(0) #Stack control using always 0
     if item_to_explore not in visited_list:
         visited_list.append(item_to_explore)
         local_path = LOCAL_PATH_PREFIX + item_to_explore[1:] + ".html"
@@ -83,18 +92,20 @@ while len(list_to_explore) > 0:
         print("List size: " + str(len(list_to_explore)))
         print("Total nodes: " + str(always_increment))
         if os.path.exists(local_path) and os.stat(local_path).st_size != 0:
-            fp = open(local_path, "r")
+            fp_item = open(local_path, "r")
+            print("Available offline")
         else:
+            print("Downloading")
             text_buffer = web_page_to_text(BASE_ALEXA_SITE + item_to_explore) 
             try:
-                fp = open_creating_dirs(local_path, "w")
-                fp.write(text_buffer)
+                fp_item = open_creating_dirs(local_path, "w")
+                fp_item.write(text_buffer)
             except: #Uninterruptable write
                 pass
-            fp.close()
-            fp = open(local_path, "r")
-        explored_result = alexa_get_subcateg_offline(fp.read())
-        fp.close()
+            fp_item.close()
+            fp_item = open(local_path, "r")
+        explored_result = alexa_get_subcateg_offline(fp_item.read())
+        fp_item.close()
         if len(explored_result) != 0:
             list_to_explore = explored_result + list_to_explore
 
